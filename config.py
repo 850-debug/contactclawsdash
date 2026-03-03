c31ef188410ec17e15ce3d99e675e894ba26815d
850-debug <immanaya850@gmail.com>
2026-03-03 14:11:27 -0800

    Create config.py file with initial configuration

---
+++ config.py
@@ -0,0 +1,24 @@
+INDUSTRIES = [
+    "Procurement",
+    "Construction",
+    "Mining",
+    "Energy",
+    "Remote Projects",
+    "Aviation Charter"
+]
+
+PROVINCES = [
+    "Quebec",
+    "Ontario",
+    "British Columbia",
+    "Alberta"
+]
+
+KEYWORDS_FR = [
+    "approvisionnement",
+    "construction",
+    "minier",
+    "énergie",
+    "ingénierie",
+    "affrètement"
+]
